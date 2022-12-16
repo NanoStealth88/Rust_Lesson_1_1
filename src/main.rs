5c56c9d610b984aada281923b9cc59e71a832dc1
NanoStealth88 <chandler.bartholomew@gmail.com>
2022-12-15 22:55:52 -0600

    Added the first print command and tested the code It didn't go to the separate line.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+     
+    // This is the first program in the course
+    // This is the second line of comment
+
+    /* This is a multiline comment
+    like with C++ */
+
+    print!("hello from Rust program!");
+    println!("   Oops i didn't use println on the previous command");
 }
