3e5900b4c0dab6956b3cf9a0f23bd83a18a778ed
NanoStealth88 <chandler.bartholomew@gmail.com>
2022-12-15 22:58:46 -0600

    Added the different print commands to get practice

--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,17 @@ fn main() {
     like with C++ */
 
     print!("hello from Rust program!");
-    println!("   Oops i didn't use println on the previous command");
+
+    // learning some basic output commands
+    println!("The value of the constant is {}",10);
+
+    //learning to print strings
+    print!("This is a print command");
+    print!("This is going to be printed on the same line");
+
+    // Learning to write on multiple lines using the print command
+    println!("This is going to be 
+    printed on multiple
+    lines");
+    
 }
